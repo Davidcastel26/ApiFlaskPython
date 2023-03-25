d947de5342e1868ca629309d184cc9dd1f29019c
David Castellanos <davcastellanoslarios@gmail.com>
2023-03-24 22:52:48 -0600

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ stores= [
 def get_stores():
     return {"stores": stores}
 
+# we are creating a store 
 @app.post("/store")
 def create_store():
     request_data = request.get_json()
@@ -25,6 +26,7 @@ def create_store():
     stores.append(new_store)
     return new_store, 201
 
+# here we are adding items into an especifict store 
 @app.post("/store/<string:name>/item")
 def create_item(name):
     request_data = request.get_json()
