5e2bc4ecfdf6510e7a41b213f85c572110057a79
David Castellanos <davcastellanoslarios@gmail.com>
2023-03-26 15:48:25 -0600

    fixing errors

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ stores= [
 def get_stores():
     return {"stores": stores}
 
-# we are creating a store 
+#we are creating a store
 @app.post("/store")
 def create_store():
     request_data = request.get_json()
@@ -26,27 +26,10 @@ def create_store():
     stores.append(new_store)
     return new_store, 201
 
-# here we are adding items into an especifict store 
 @app.post("/store/<string:name>/item")
 def create_item(name):
     request_data = request.get_json()
     for store in stores:
         if store["name"] == name:
-            new_item = {"name":request_data["name"],"price": request_data["price"]}
-            store["items"].append(new_item)
-            return new_item, 201
-    return {"message":"Store not found"}, 404
+            new_item = {"name": request_data["name"], "price": request_data["price"]}
 
-@app.get("store/<string: name>")
-def get_store(name):
-    for store in stores:
-        if store["name"] == name:
-            return store
-    return {"message":"Store not found"}, 404
-
-@app.get("/store/<string: name>/item")
-def get_item_in_store(name):
-    for store in stores:
-        if store["name"] == name:
-            return {"items": store["items"]}
-    return {"message":"Store not found"}, 404
