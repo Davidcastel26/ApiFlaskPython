df36cddfb1bd8a87f7e4585ebae8e50512e2c91e
David Castellanos <davcastellanoslarios@gmail.com>
2023-04-04 21:33:15 -0600

    app get "/item"

--- app.py
+++ app.py
@@ -29,19 +29,11 @@ def create_item():
     item = {**item_data, "id":item_id}
     items[item_id] = item
     return item, 201
-    # for store in stores:
-    #     if store["name"] == name:
-    #         new_item = {"name": request_data["name"], "price": request_data["price"]}
-    #         store["items"].append(new_item)
-    #         return new_item, 201
-    # return {"message":"Store not found"}, 404
 
-# @app.get("/store/<string:name>")
-# def get_store(name):
-#     for store in stores:
-#         if store["name"] == name:
-#             return store
-#     return {"message":"Store not found"}, 404
+@app.get("/item") #http://127.0.0.1:5000/store
+def get_all_items():
+    return {"items": list(items.values())}
+
 
 @app.get("/store/<string:store_id>")
 def get_store(store_id):
